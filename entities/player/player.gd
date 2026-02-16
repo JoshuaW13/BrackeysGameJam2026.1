fe750019fd33e1999a3b68bb54f4c4ad4f6047a9
JoshuaW13 <josh.walters@shaw.ca>
2026-02-15 21:49:23 -0700

    Made janky box

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -5,6 +5,8 @@ class_name Player
 @onready var velocity_component : VelocityComponent = %VelocityComponent
 @onready var gravity_component: GravityComponent = %GravityComponent
 
+var push_strength: float = 5000
+
 func _ready() -> void:
 	state_machine.start()
 
@@ -12,3 +14,11 @@ func _physics_process(delta):
 	if not is_on_floor():
 		gravity_component.apply(delta)
 	velocity_component.apply(self)
+	for i in range(get_slide_collision_count()):
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		
+		if collider is RigidBody2D:
+			var push_dir = collision.get_normal()
+			var horizontal_dir = Vector2(-push_dir.x, 0).normalized()
+			collider.apply_central_impulse(horizontal_dir * push_strength * delta)
--- entities/player/states/idle.gd
+++ entities/player/states/idle.gd
@@ -3,9 +3,6 @@ class_name Idle
 
 @export var player: Player
 
-func enter()->void:
-	print("Entering idle")
-
 func physics_update(_delta: float)-> void:
 	if Input.is_action_pressed("left") or Input.is_action_pressed("right") or Input.is_action_pressed("up") or Input.is_action_pressed("down"):
 		transition.emit(self, "Walk")
