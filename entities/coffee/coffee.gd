6be877f20d711ae01661a60fc5f187a68d0f925d
JoshuaW13 <josh.walters@shaw.ca>
2026-02-16 21:42:10 -0700

    Fix coffee sprite position

--- entities/coffee/coffee.gd
+++ entities/coffee/coffee.gd
@@ -4,3 +4,7 @@ class_name Coffee
 func _ready() -> void:
 	inventory_item = load("res://entities/coffee/coffee.tres")
 	super._ready()
+
+func _integrate_forces(state):
+	var vel = state.get_linear_velocity()
+	state.set_linear_velocity(Vector2(0, vel.y))
