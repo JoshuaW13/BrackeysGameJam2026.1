bb0681e1122c7fc79e3c4e98425526e9d337eefd
Nick40831 <nick40831@hotmail.com>
2026-02-17 18:55:47 -0700

    Limited Pickup Distance
    Don't know enough to make line of sight detection.

--- entities/placeableItem/placeableItem.gd
+++ entities/placeableItem/placeableItem.gd
@@ -9,4 +9,15 @@ func _ready() -> void:
 
 func _input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action("take"):
-		SignalBus.item_picked_up.emit(self)
+		if check_los():
+			SignalBus.item_picked_up.emit(self)
+
+func check_los():
+	var player = get_tree().get_first_node_in_group("player")
+	if player.global_position.distance_to(global_position) > 30:
+		print("Item too far")
+		return false
+
+	# Could not get line of sight working, tried it for fun. But did limit distance.
+		
+	return true
