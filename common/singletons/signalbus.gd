c5f57818848f0895824fb14610709e9f4adef368
JoshuaW13 <josh.walters@shaw.ca>
2026-02-19 18:04:17 -0700

    Deleted level 2 and made coffee topping addition

--- common/singletons/signalbus.gd
+++ common/singletons/signalbus.gd
@@ -1,5 +1,6 @@
 extends Node
 
 signal item_picked_up(item: PlaceableItem)
+signal topping_picked_up(topping: Topping)
 
 signal unlock_door(door_id : String)
--- levels/level2/manager.gd
+++
@@ -1,1 +0,0 @@
-extends "res://entities/npc/npc.gd"
