d3daf71a3b6aa50f3ca8c44eea64d13abff9df50
JoshuaW13 <josh.walters@shaw.ca>
2026-02-19 23:16:54 -0700

    Fixed back button

--- userInterface/screens/levelSelect/level_select.gd
+++ userInterface/screens/levelSelect/level_select.gd
@@ -7,15 +7,16 @@ func _ready() -> void:
 	for i in range(levels.size()+1):
 		var elevator_button_scene : PackedScene = load("res://userInterface/components/elevatorButton/ElevatorButton.tscn")
 		var elevator_button : ElevatorButton= elevator_button_scene.instantiate()
-		if i ==0:
+		if i==0:
 			elevator_button.text = str("M")
 		else:
 			elevator_button.text = str(i)
 		level_selection_grid.add_child(elevator_button)
-		elevator_button.button.button_down.connect(func():
-			get_tree().change_scene_to_packed(levels[i-1])
+		if i ==0:
+			elevator_button.button.button_down.connect(func():
+				get_tree().change_scene_to_file("res://userInterface/screens/titleScreen/TitleScreen.tscn")
 			)
-
-
-func _on_button_button_down() -> void:
-	get_tree().change_scene_to_file("res://userInterface/screens/titleScreen/TitleScreen.tscn")
+		else:
+			elevator_button.button.button_down.connect(func():
+				get_tree().change_scene_to_packed(levels[i-1])
+			)
