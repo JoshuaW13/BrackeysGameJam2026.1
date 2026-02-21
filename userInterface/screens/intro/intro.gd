a66ed3fcc03333a8f667c9329f5bc04ac44b6a21
jennavlaar <100974543+jennavlaar@users.noreply.github.com>
2026-02-20 23:18:15 -0700

    Fixing intro transition
    - Broke no longer

--- userInterface/screens/intro/intro.gd
+++ userInterface/screens/intro/intro.gd
@@ -7,9 +7,7 @@ const LEVEL_SELECT_THEME = preload("res://audio/music/levels_1.ogg")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GlobalAudio.play_music(LEVEL_SELECT_THEME)
-	animation_player.animation_finished.connect(_animation_finished)
 	animation_player.play("cutscene")
 
 func _animation_finished() -> void:
-	print("all done")
-	# Credits
+	get_tree().change_scene_to_file("res://userInterface/screens/levelSelect/LevelSelect.tscn")
