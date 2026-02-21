1fe7c20b72c32382a300f9131da8ab6b036856a7
jennavlaar <100974543+jennavlaar@users.noreply.github.com>
2026-02-20 19:15:56 -0700

    Playing cutscene when game launches

---
+++ userInterface/screens/intro/intro.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animation_player.animation_finished.connect(_animation_finished)
+	animation_player.play("cutscene")
+
+func _animation_finished() -> void:
+	get_tree().change_scene_to_file("res://userInterface/screens/levelSelect/LevelSelect.tscn")
--- userInterface/screens/titleScreen/title_screen.gd
+++ userInterface/screens/titleScreen/title_screen.gd
@@ -2,4 +2,4 @@ extends Control
 
 
 func _on_play_button_down() -> void:
-	get_tree().change_scene_to_file("res://userInterface/screens/levelSelect/LevelSelect.tscn")
+	get_tree().change_scene_to_file("res://userInterface/screens/intro/Intro.tscn")
