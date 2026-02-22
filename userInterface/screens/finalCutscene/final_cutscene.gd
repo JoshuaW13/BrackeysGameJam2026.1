4caa9874c767e79425fab780b7fe140000ed4064
jennavlaar <100974543+jennavlaar@users.noreply.github.com>
2026-02-21 19:08:15 -0700

    Finishing final cutscene
    - Added audio and dialogue
    - Redid timing to match dialogue

--- userInterface/screens/finalCutscene/final_cutscene.gd
+++ userInterface/screens/finalCutscene/final_cutscene.gd
@@ -1,11 +1,27 @@
 extends Node2D
 
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var skip_button : Button = $SkipButton
 
-# Called when the node enters the scene tree for the first time.
+var OPEN_SOUND = load("res://audio/elevator open.wav")
+var CLOSE_SOUND = load("res://audio/elevator close.wav")
+var FINAL_LEVEL_THEME = load("res://audio/music/levels_1.ogg")
+
 func _ready() -> void:
+	GlobalAudio.play_music(FINAL_LEVEL_THEME)
 	animation_player.play("cutscene")
 
+func _play_dialogue_sound(blips: int) -> void:
+	GlobalAudio.play_dialogue_fx(blips)
+	
+func _play_elevator_sound(open: bool) -> void:
+	if open:
+		GlobalAudio.play_world_fx(OPEN_SOUND)
+	else:
+		GlobalAudio.play_world_fx(CLOSE_SOUND)
+
 func _animation_finished() -> void:
-	# ROLL CREDITS
+	_load_credits()
+
+func _load_credits() -> void:
 	get_tree().change_scene_to_file("res://userInterface/screens/levelSelect/LevelSelect.tscn")
