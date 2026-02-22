5e1f51898549e59f8a2bd77170b601d59f939e0e
JoshuaW13 <josh.walters@shaw.ca>
2026-02-21 21:22:38 -0700

    fix dailogue panel theme override stuff

--- entities/dialogue/DialoguePanel.gd
+++ entities/dialogue/DialoguePanel.gd
@@ -2,11 +2,11 @@ extends Control
 class_name DialoguePanel
 
 @onready var panel : Control = self
-@onready var sprite : Sprite2D = $Sprite2D
+@onready var panel_container : PanelContainer = %PanelContainer
 @onready var dialogue: Label = %TextLabel
 @onready var name_label : Label = %NameLabel
-@onready var dialogue_sprite : Texture = load("res://entities/dialogue/DialoguePanel.png")
-@onready var message_sprite : Texture = load("res://entities/dialogue/MessageBox.png")
+@onready var dialogue_style: StyleBoxTexture = StyleBoxTexture.new()
+@onready var message_style: StyleBoxTexture = StyleBoxTexture.new()
 signal dialogue_finished(npc_id)
 
 var dialogue_lines: Array = []
@@ -15,6 +15,11 @@ var ignore_next_input := false
 var npc : String
 
 func _ready() -> void:
+	dialogue_style.texture = load("res://entities/dialogue/DialoguePanel.png")
+	dialogue_style.draw_center = true 
+	
+	message_style.texture = load("res://entities/dialogue/MessageBox.png")
+	message_style.draw_center = true
 	var npcs = get_tree().get_nodes_in_group("npc")
 	for npc in npcs:
 		if npc is NPC:
@@ -27,10 +32,10 @@ func _on_npc_dialogue(npc_id, name, lines):
 	npc = npc_id
 	
 	if npc_id == "message":
-		#sprite.texture = message_sprite
+		panel_container.add_theme_stylebox_override("panel", message_style)
 		name_label.text = ""
 	else:
-		#sprite.texture = dialogue_sprite
+		panel_container.add_theme_stylebox_override("panel", dialogue_style)
 		name_label.text = name
 	
 	if lines.is_empty():
